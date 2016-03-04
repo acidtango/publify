401fd6885f2cfa2e73ad775cc719b0c8027b7f44
Juan F. Nogueras <juan@acidtango.com>
2016-03-04 19:35:18 +0100

    Fix error in deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :bundle_without, [:darwin, :development, :test]
 set :deploy_to, "/opt/acid_tango_blog"
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push("config/database.yml config/mail.yml")
+set :linked_files, fetch(:linked_files, []).push("config/database.yml", "config/mail.yml")
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, [])
